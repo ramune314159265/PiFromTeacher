c7fe035f25bd10a006b0b627059c84edbbbf4ef0
ramune314159265 <ramune314159265@gmail.com>
2025-05-07 22:54:27 +0900

    合計とか表示するように

--- src/components/SeatsInfo.jsx
+++ src/components/SeatsInfo.jsx
@@ -1,19 +1,23 @@
 import { DataListItem, DataListItemLabel, DataListItemValue, DataListRoot } from '@chakra-ui/react'
+import { useSeatsData } from '../atoms/seatsData'
 
 export const SeatsInfo = () => {
+	const [seatsData] = useSeatsData()
+	const seatDataFlat = seatsData.flat()
+
 	return (
-		<DataListRoot orientation="horizontal" size="lg" w="full">
+		<DataListRoot orientation="horizontal" size="md" w="full">
 			<DataListItem>
 				<DataListItemLabel>合計</DataListItemLabel>
-				<DataListItemValue>1</DataListItemValue>
+				<DataListItemValue>{seatDataFlat.reduce((p, c) => p + c)}</DataListItemValue>
 			</DataListItem>
 			<DataListItem>
 				<DataListItemLabel>平均</DataListItemLabel>
-				<DataListItemValue>1</DataListItemValue>
+				<DataListItemValue>{seatDataFlat.reduce((p, c) => p + c) / seatDataFlat.length}</DataListItemValue>
 			</DataListItem>
 			<DataListItem>
 				<DataListItemLabel>最大値</DataListItemLabel>
-				<DataListItemValue>1</DataListItemValue>
+				<DataListItemValue>{Math.max(...seatDataFlat)}</DataListItemValue>
 			</DataListItem>
 		</DataListRoot>
 	)

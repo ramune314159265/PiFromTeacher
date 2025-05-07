536fc921269e197a454d3202ec00df64919b18c9
ramune314159265 <ramune314159265@gmail.com>
2025-05-07 23:00:35 +0900

    背景色追加

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,4 +1,4 @@
-import { NumberInputControl, NumberInputInput, NumberInputRoot } from '@chakra-ui/react'
+import { Box, NumberInputControl, NumberInputInput, NumberInputRoot } from '@chakra-ui/react'
 import { useSeatsData } from '../atoms/seatsData'
 
 export const Seat = ({ x, y }) => {
@@ -6,19 +6,26 @@ export const Seat = ({ x, y }) => {
 
 	const maxSeatValue = Math.max(...seatsData.flat())
 	return (
-		<NumberInputRoot
+		<Box
 			w="full"
 			h="full"
 			borderRadius="md"
-			style={{ backgroundColor: `rgb(255 0 0 / ${seatsData[y][x] / maxSeatValue})` }}
-			size="xs"
-			min={0}
-			max={100}
-			value={seatsData[y][x]}
-			onValueChange={e => setSeatValue({ x, y, value: parseInt(e.value) })}
+			background="bg"
 		>
-			<NumberInputControl></NumberInputControl>
-			<NumberInputInput h="full"></NumberInputInput>
-		</NumberInputRoot>
+			<NumberInputRoot
+				w="full"
+				h="full"
+				borderRadius="md"
+				style={{ backgroundColor: `rgb(255 0 0 / ${seatsData[y][x] / maxSeatValue})` }}
+				size="sm"
+				min={0}
+				max={100}
+				value={seatsData[y][x]}
+				onValueChange={e => setSeatValue({ x, y, value: parseInt(e.value) })}
+			>
+				<NumberInputControl></NumberInputControl>
+				<NumberInputInput h="full"></NumberInputInput>
+			</NumberInputRoot>
+		</Box>
 	)
 }
--- src/components/SeatsData.jsx
+++ src/components/SeatsData.jsx
@@ -3,7 +3,16 @@ import { Seat } from './Seat'
 
 export const SeatsData = ({ width, height }) => {
 	return (
-		<Grid templateRows={`repeat(${height}, 1fr)`} templateColumns={`repeat(${width}, 1fr)`} p={1} gap={1} w="400px" h="300px">
+		<Grid
+			templateRows={`repeat(${height}, 1fr)`}
+			templateColumns={`repeat(${width}, 1fr)`}
+			p={2}
+			gap={1}
+			w="400px"
+			h="300px"
+			background="bg.subtle"
+			borderRadius="md"
+		>
 			{
 				Array(width * height).fill(0).map((_, i) => {
 					return (

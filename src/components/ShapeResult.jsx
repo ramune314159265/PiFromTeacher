2dd9e99347c92c41314e47464fff800caed23a7d
ramune314159265 <ramune314159265@gmail.com>
2025-05-10 00:03:13 +0900

    キャンバスに点を描画するように

--- src/components/ShapeResult.jsx
+++ src/components/ShapeResult.jsx
@@ -1,13 +1,56 @@
 import { DataListItem, DataListItemLabel, DataListItemValue, DataListRoot, Flex, Heading } from '@chakra-ui/react'
+import { useEffect, useRef } from 'react'
 import { useDotPoses } from '../atoms/dotPoses'
+import { useSeatsData } from '../atoms/seatsData'
 
 export const ShapeResult = ({ shape }) => {
+	const [seatsData] = useSeatsData()
 	const [dotPoses, { setDotPoses }] = useDotPoses()
-	const dotTestPassed = dotPoses.filter(e => shape.test(...e))
+	const dotPosesWithTestPassed = dotPoses.map(e => [...e, shape.test(...e)])
+	const dotTestPassed = dotPosesWithTestPassed.filter(e => e[2])
+
+	const canvasRef = useRef(null)
+	useEffect(() => {
+		if (!canvasRef.current) {
+			return
+		}
+		/** @type {HTMLCanvasElement} */
+		const canvas = canvasRef.current
+
+		const size = 300
+		canvas.width = size * devicePixelRatio
+		canvas.height = size * devicePixelRatio
+		canvas.style.width = '300px'
+		canvas.style.height = '300px'
+
+		const ctx = canvas.getContext('2d', { alpha: false })
+		ctx.scale(devicePixelRatio, devicePixelRatio)
+
+		ctx.fillStyle = 'white'
+		ctx.fillRect(0, 0, size, size)
+
+		ctx.strokeStyle = 'black'
+		seatsData.forEach((line, y) => {
+			const width = line.length
+			const height = seatsData.length
+			line.forEach((_, x) => {
+				ctx.strokeRect(size / width * x, size / height * y, size / width, size / height)
+			})
+		})
+
+		dotPosesWithTestPassed.forEach(([x, y, testPassed]) => {
+			ctx.fillStyle = testPassed ? 'green' : 'red'
+			ctx.beginPath()
+			ctx.arc(x * size, y * size, 4, 0, 2 * Math.PI)
+			ctx.closePath()
+			ctx.fill()
+		})
+	}, [dotPosesWithTestPassed, seatsData])
 
 	return (
 		<Flex direction="column" w="300px" h="full" gap={4}>
 			<Heading size="xl">{shape.name}</Heading>
+			<canvas ref={canvasRef}></canvas>
 			<DataListRoot orientation="horizontal" size="md" w="full">
 				<DataListItem>
 					<DataListItemLabel>円の中の点</DataListItemLabel>

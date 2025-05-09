2da182bb4b975a263f16a4d3dfdab22dda1fc2c7
ramune314159265 <ramune314159265@gmail.com>
2025-05-10 00:21:32 +0900

    座席データを保存するように

--- src/atoms/seatsData.js
+++ src/atoms/seatsData.js
@@ -1,9 +1,17 @@
 import { atom, useAtom } from 'jotai'
+import { useEffect } from 'react'
 
-export const seatsDataAtom = atom(Array(6).fill(0).map(_ => Array(6).fill(0)))
+export const seatsDataAtom = atom(localStorage.getItem('ramune314159265.piFromTeacher.seatsData') === null ?
+	Array(6).fill(0).map(_ => Array(6).fill(0)) :
+	JSON.parse(localStorage.getItem('ramune314159265.piFromTeacher.seatsData'))
+)
 
 export const useSeatsData = () => {
 	const [seatsData, setSeatsData] = useAtom(seatsDataAtom)
+
+	useEffect(() => {
+		localStorage.setItem('ramune314159265.piFromTeacher.seatsData', JSON.stringify(seatsData))
+	}, [seatsData])
 
 	const setSeatValue = ({ x, y, value }) => {
 		const data = structuredClone(seatsData)
